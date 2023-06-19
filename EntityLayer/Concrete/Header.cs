6cd5c13ddc489e82c4f788fe926c4172f5c46428
Halil_İbrahim_Morgil <halilibrahimmrgl@gmail.com>
2023-06-19 23:56:25 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Header.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Header
+    {
+        [Key]
+        public int HeaderId { get; set; }
+
+        [StringLength(50)]
+        public string HeaderName { get; set; }
+        public DateTime HeaderDate { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+        public ICollection<Content> Contents { get; set; }
+    }
+}
