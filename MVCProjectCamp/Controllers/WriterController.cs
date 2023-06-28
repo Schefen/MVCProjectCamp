5429575f60078871cdd774c71741e7b7329f7bf1
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-06-28 22:18:44 +0300

    Writer işlemleri

---
+++ MVCProjectCamp/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager _writerManager = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var WriterValues = _writerManager.GetList();
+            return View(WriterValues);
+        }
+    }
+}
