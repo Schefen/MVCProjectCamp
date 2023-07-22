4ec93bb593c8ad3b6bfa00bf8cc9fae31a646f81
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-07-22 20:01:51 +0300

    Değişiklikler

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.ContactMail).NotEmpty().WithMessage("Mail Adresi Boş Bırakılamaz");
+            RuleFor(x => x.ContactSubject).NotEmpty().WithMessage("Konu Adı Boş Bırakılamaz");
+            RuleFor(x => x.ContactUsername).NotEmpty().WithMessage("Kullanıcı Adı Boş Bırakılamaz");
+            RuleFor(x => x.ContactSubject).MinimumLength(3).WithMessage("En Az 3 Karakter Girilmelidir");
+            RuleFor(x => x.ContactUsername).MinimumLength(3).WithMessage("En Az 3 Karakter Girilmelidir");
+            RuleFor(x => x.ContactSubject).MaximumLength(50).WithMessage("Konu Uzunluğu Max 50 Karakter Olmalıdır");
+            RuleFor(x => x.ContactMessage).MinimumLength(18).WithMessage("Mesaj Min 18 Karakter Olmalıdır");
+        }
+    }
+}
--- MVCProjectCamp/Controllers/ContactController.cs
+++ MVCProjectCamp/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,18 @@ namespace MVCProjectCamp.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager _contactManager = new ContactManager(new EfContactDal());
+        ContactValidator _contactValidator = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contactValues = _contactManager.GetList();
+            return View(contactValues);
         }
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues = _contactManager.GetById(id);
+            return View(contactValues);
+        }
+
     }
 }
