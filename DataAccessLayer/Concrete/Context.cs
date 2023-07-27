5c7c54d6a3b8148abe0cfdfc38f34f4459852245
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-07-27 15:55:36 +0300

    Some changes

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -17,5 +17,8 @@ namespace DataAccessLayer.Concrete
         public DbSet<Header> Headers { get; set; }
         public DbSet<Writer> Writers { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<ImageFile> ImageFıles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
     }
 }
---
+++ MVCProjectCamp/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        ImageFileManager _imageFileManager = new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var files = _imageFileManager.GetList();
+            return View(files);
+        }
+    }
+}
