f26838dd55a87c2337ff09d7b9217922567763a8
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-08-23 01:47:50 +0300

    Recap 94

---
+++ MVCProjectCamp/Controllers/ChartController.cs
@@ -0,0 +1,52 @@
+﻿using MVCProjectCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(),JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult ContentChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+        public List<Categories> BlogList()
+        {
+            List<Categories> categories = new List<Categories>();
+            categories.Add(new Categories
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 8
+            });     
+            categories.Add(new Categories
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });        
+            categories.Add(new Categories
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });  
+            categories.Add(new Categories
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return categories;
+        }
+    }
+}
---
+++ MVCProjectCamp/Models/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProjectCamp.Models
+{
+    public class Categories
+    {
+        public string CategoryName { get; set; }
+        public int CategoryCount { get; set; }
+    }
+}
